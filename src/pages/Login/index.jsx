f57cb03ae19fca7deb7e8df936f78855dadd03bf
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-06-19 18:53:04 -0300

    * New login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { UserLogin } from "../../redux/actions/UserAction";
+import logo from "./images/logo.png";
 import "./styles/Login.css";
-import logo from './images/logo.png' 
+import "./styles/Login-mobile.css";
 
 class Login extends Component {
   constructor() {
@@ -23,26 +24,36 @@ class Login extends Component {
     const minLength = 3;
     return (
       <div className="page-login">
-        <div className="container-logo">
-          <img src={logo} alt="logo" />
-        </div>
-        <form method="get">
-          <input
-            placeholder="Username"
-            type="text"
-            name="name"
-            onChange={onInputChange}
-            value={name}
-          />
-          <button
-            type="button"
-            disabled={name.length < minLength}
-            onClick={() => {
-              dispatch(UserLogin(true, name));
-            }}
-          >
-            Login
-          </button>
+        <section className="banner-login">
+          <div>
+            <span>✦ biewwl</span>
+            <h1>Listen</h1>
+            <p>Enjoy your favorite songs. Save and listen wherever you want!</p>
+            <label htmlFor="name">Let's start</label>
+          </div>
+        </section>
+        <form method="get" className="form-login">
+          <div>
+            <img src={logo} alt="logo" className="logo-login" />
+            <span>Login</span>
+            <input
+              placeholder="Username"
+              type="text"
+              name="name"
+              id="name"
+              onChange={onInputChange}
+              value={name}
+            />
+            <button
+              type="button"
+              disabled={name.length < minLength}
+              onClick={() => {
+                dispatch(UserLogin(true, name));
+              }}
+            >
+              Login
+            </button>
+          </div>
         </form>
       </div>
     );
